4c75c39bda55be09281acd7c70b6a3e6869d3bc8
Gaurav Khadka <gauravkhadka111111@gmail.com>
2025-01-17 02:25:44 +0545

    fixed dashboard error

--- app/(routes)/dashboard/page.jsx
+++ app/(routes)/dashboard/page.jsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { UserButton, useUser } from "@clerk/nextjs";
 import React, { useEffect, useState } from "react";
 import CardInfo from "./_components/CardInfo";
@@ -9,20 +10,22 @@ import { desc, eq, getTableColumns, sql } from "drizzle-orm";
 import BudgetItem from "./budgets/_components/BudgetItem";
 import ExpensesListTable from "./expenses/_components/ExpensesListTable";
 
-function Dashboard({ children }) {
+function Dashboard() {
   const { user } = useUser();
 
   const [budgetList, setBudgetList] = useState([]);
-  const [expensesList,setExpensesList]=useState([]);
+  const [expensesList, setExpensesList] = useState([]);
+
   useEffect(() => {
-    user && getBudgetList();
+    if (user) {
+      getBudgetList();
+    }
   }, [user]);
 
   const getBudgetList = async () => {
     const result = await db
       .select({
         ...getTableColumns(Budgets),
-
         totalSpend: sql`sum(${Expenses.amount})`.mapWith(Number),
         totalItem: sql`count(${Expenses.id})`.mapWith(Number),
       })
@@ -31,44 +34,49 @@ function Dashboard({ children }) {
       .where(eq(Budgets.createdBy, user?.primaryEmailAddress?.emailAddress))
       .groupBy(Budgets.id)
       .orderBy(desc(Budgets.id));
+
     setBudgetList(result);
     getAllExpenses();
-  }
-  const getAllExpenses=async()=>{
-    const result=await db.select({
-      id:Expenses.id,
-      name:Expenses.name,
-      amount:Expenses.amount,
-      createdAt: Expenses.createdAt
-    }).from(Budgets)
-    .rightJoin(Expenses,eq(Budgets.id,Expenses.budgetId))
-    .where(eq(Budgets.createdBy,user?.primaryEmailAddress.emailAddress))
-    .orderBy(desc(Expenses.id));
+  };
+
+  const getAllExpenses = async () => {
+    const result = await db
+      .select({
+        id: Expenses.id,
+        name: Expenses.name,
+        amount: Expenses.amount,
+        createdAt: Expenses.createdAt,
+      })
+      .from(Budgets)
+      .rightJoin(Expenses, eq(Budgets.id, Expenses.budgetId))
+      .where(eq(Budgets.createdBy, user?.primaryEmailAddress.emailAddress))
+      .orderBy(desc(Expenses.id));
+
     setExpensesList(result);
     console.log(result);
-  }
+  };
 
   return (
     <div className="p-8">
       <h2 className="font-bold text-3xl">Hi, {user?.firstName}</h2>
       <p className="text-gray-500">
-        Here's what happening with you money, Lets Manage your expense.
+        Here's what happening with your money. Let's manage your expenses.
       </p>
       <CardInfo budgetList={budgetList} />
       <div className="grid grid-cols-1 md:grid-cols-3 mt-6 gap-5">
         <div className="md:col-span-2">
           <BarChartDashboard budgetList={budgetList} />
           <ExpensesListTable
-          expensesList={expensesList}
-          refreshData={()=>getBudgetList()}
+            expensesList={expensesList}
+            refreshData={() => getBudgetList()}
           />
         </div>
         <div className="grid gap-5">
           <h2 className="font-bold text-lg">Latest Budget</h2>
-          {budgetList.map((budget,index)=>(
-            <BudgetItem budget={budget} key={index}/>
+          {budgetList.map((budget, index) => (
+            <BudgetItem budget={budget} key={index} />
           ))}
-          </div>
+        </div>
       </div>
     </div>
   );
